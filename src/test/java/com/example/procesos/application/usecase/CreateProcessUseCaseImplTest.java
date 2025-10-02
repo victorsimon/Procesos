bd1f7532ffa57a270272f047cbe199f298f30b51
vsimon <vsimon.batanero@gmail.com>
2025-10-02 18:06:20 +0200

    Test

--- src/test/java/com/example/procesos/application/usecase/CreateProcessUseCaseImplTest.java
+++ src/test/java/com/example/procesos/application/usecase/CreateProcessUseCaseImplTest.java
@@ -1,18 +1,34 @@
 package com.example.procesos.application.usecase;
 
+import com.example.procesos.application.util.validator.ProcessAllwaysTrueValidator;
 import com.example.procesos.domain.Process;
+import com.example.procesos.domain.port.out.ProcessRepository;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateProcessUseCaseImplTest {
 
+    private final ProcessAllwaysTrueValidator processValidator = new ProcessAllwaysTrueValidator();
+    private final ProcessRepository processRepository = new ProcessRepository() {
+        @Override
+        public Process save(Process process) {
+            return process;
+        }
+
+        @Override
+        public List<Process> findAll() {
+            return List.of();
+        }
+    };
+
     @Test
     void createHappyCase() {
         // Given
-        CreateProcessUseCaseImpl sut = new CreateProcessUseCaseImpl();
+        CreateProcessUseCaseImpl sut = new CreateProcessUseCaseImpl(processRepository, processValidator);
         LocalDateTime dateTime = LocalDateTime.now();
 
         // When
